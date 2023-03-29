73980348260b72e8ea1027eeab539dd0ac4bf52e
Miguel Martinez <miguel.danto@gmail.com>
2023-03-28 18:07:46 -0600

    agregando globalthis en eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,4 +19,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
+  globals: {
+    globalThis: 'readonly',
+  },
 }
