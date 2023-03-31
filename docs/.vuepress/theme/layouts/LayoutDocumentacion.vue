40168adccf68219bae871f143404d8d8b75ca3a6
jdanielgoh <48138542+jdanielgoh@users.noreply.github.com>
2023-03-31 15:55:16 -0600

    Adecuando el indicedecontenido al layout

--- docs/.vuepress/theme/layouts/LayoutDocumentacion.vue
+++ docs/.vuepress/theme/layouts/LayoutDocumentacion.vue
@@ -73,7 +73,11 @@
             <Content />
           </div>
           <div class="columna-4">
-            <!--<IndiceDeContenido class="indice-contenido-documentacion">
+            <IndiceDeContenido
+              class="indice-contenido-documentacion"
+              :id_indice="'indice-template'"
+              ref="componenteIndice"
+            >
               <template slot="contenido-indice-de-contenido">
                 <ul>
                   <li
@@ -85,33 +89,50 @@
                 </ul>
               </template>
             </IndiceDeContenido>
-            -->
           </div>
         </div>
       </div>
     </div>
   </div>
 </template>
-<!--
 <script setup>
-import { ref, onMounted } from 'vue'
+import { useRoute } from 'vue2-helpers/vue-router'
+
+import { ref, onMounted, watch } from 'vue'
 const lista_elementos = ref([])
 
+const route = useRoute()
+const componenteIndice = ref()
+
 onMounted(() => {
+  setTimeout(() => actualizaContenidoIndice(), 200)
+})
+
+watch(route, () => {
+  let elementos = []
+  setTimeout(() => actualizaContenidoIndice(), 200)
+})
+function actualizaContenidoIndice() {
   let elementos = []
   document.querySelectorAll('div.content__default h2').forEach(el => {
-    console.log(el.id)
     if (el.id) {
       elementos.push({
         id: el.id,
-        texto: el.innerText,
+        texto: el.innerText.replace('#', ''),
       })
     }
   })
   lista_elementos.value = elementos
-})
+  /**
+   * Apuramos al componente para que su lista de elementos se actualice y
+   * obtenemos las rutas
+   */
+  componenteIndice.value._setupState.lista_elementos.value =
+    lista_elementos.value
+  componenteIndice.value._setupState.seccion_visible.value = ''
+  componenteIndice.value._setupState.autoScrollSuave()
+}
 </script>
--->
 
 <style src="prismjs/themes/prism-tomorrow.css"></style>
 <style>
--- docs/documentacion/indice-de-contenido/index.md
+++ docs/documentacion/indice-de-contenido/index.md
@@ -23,6 +23,12 @@ Uso:
 - `titulo`: (_String_) Corresponde al texto que se muestra como encabezado del componente. Por _default_ tiene el valor 'Contenido'.
 - `altura`: (_Number_) Este valor corresponde al la distancia en pixeles entre la orilla superior de la ventana y el inicio de un elemento de la página a los cuales hacen referencia los enlaces del índice. Al hacer click en uno de los enlaces del índice, el contenido se desplaza de forma que el elemento referenciado coindida con el borde superior de la ventana más esta cantidad `altura`. Si no se tomara en cuenta esta propiedad, la barra de navegación superior quedaría encima del inicio del elemento seleccionado. Su valor por _default_ es 50
 
+### Métodos
+
+- `autoScrollSuave` Este metodo selecciona los enlaces del componente y les especifica a que posición se debe mover el documento cuando se clickea un enlace, que es la posición del elemento referenciado menos `props.altura`.
+- `scroleando` Cada que se scrolea el documento, se verifica si el límite superior e inferior de algun elemento referenciado coindice con la posición superior de la ventana más `props.altura`. En caso verdadero, se añade la clase `link-activo` al indice asociado al elemento referenciado
+- `obtenerRutas` Obtiene la lista de `href`s en el índice
+
 ### Slots
 
 - `'contenido-indice-de-contenido'`: Este es el único slot que tiene este componente. En él se deben introducir elementos de enlaces como `<a></a>` o `<router-link>`. Los enlaces que se introducen deben contener el id del elemento al que se quiere navegar, y a su vez, este elemento debe existir en el documento.
