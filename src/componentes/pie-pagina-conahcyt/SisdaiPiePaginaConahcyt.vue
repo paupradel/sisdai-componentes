6cd069a15de299965c901494c6b7dc2f61258c3c
ciretorres <erictorres.velasco@gmail.com>
2023-05-22 15:07:23 -0600

    cambiando el apartado de derechos reservados en SisdaiPiePaginaConahcyt

--- src/componentes/pie-pagina-conahcyt/SisdaiPiePaginaConahcyt.vue
+++ src/componentes/pie-pagina-conahcyt/SisdaiPiePaginaConahcyt.vue
@@ -15,7 +15,7 @@
           Alcaldía Benito Juárez, C.P. 03940, Ciudad de México<br />
           Tel: (55) 5322-7700
         </p>
-        <p>©2021 Conahcyt Derechos Reservados</p>
+        <p>©2023 Conahcyt Derechos Reservados en trámite</p>
       </div>
       <nav class="columna-8 columna-4-esc">
         <p class="titulo-lista">Enlaces</p>
